c5a99f36f8f39529710efc88cf90d763d7c49fe3
ram kaushal <ramkaushalguralla@gmail.com>
2022-03-01 02:20:03 +0530

    processsing details func added

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+
+import pandas as pd
+from sklearn import preprocessing
 class PCA:
     def __init__(self,path):
         self.path = path 
+    def read_file(self):
+        df = pd.read_csv("https://raw.githubusercontent.com/akshayg056/Epileptic-seizure-detection-/master/data.csv")
+        return df 
+    def preprocessing(dataframe):
+
+
+        
+
     
 
     
